bffd766e1d39759d15d404287f256113aa207f8c
Vinicius Cerqueira <viniciuscerqueira@MacBook-Air-de-Vinicius.local>
2025-04-10 10:53:52 -0300

    add navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals_secao8/data/dummy_data.dart';
 import 'package:meals_secao8/screens/categories.dart';
-import 'package:meals_secao8/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -24,7 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: theme,
-        home: const MealsScreen(title: 'Some category', meals: dummyMeals),
+        home: const CategoriesScreen(),
     );
   }
 }
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals_secao8/data/dummy_data.dart';
+import 'package:meals_secao8/screens/meals.dart';
+import 'package:meals_secao8/models/category.dart';
 import 'package:meals_secao8/widgets/category_grid_item.dart';
-
-
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+            title: category.title,
+            meals: filteredMeals,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +35,9 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(category: category, onSelectCategory: (){
+              _selectCategory(context, category);
+            },),
         ],
       ),
     );
